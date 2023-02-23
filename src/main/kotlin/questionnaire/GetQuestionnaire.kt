f5374629b889209cffab1689faf3d53228b384a7
Youngbok Yoon <bok@weltcorp.com>
2023-02-23 13:56:21 +0900

    [#10] cleanup

--- src/main/kotlin/questionnaire/GetQuestionnaire.kt
+++ src/main/kotlin/questionnaire/GetQuestionnaire.kt
@@ -8,7 +8,7 @@ suspend fun main(args: Array<String>) {
     val config = QuestionnaireApiConfig.Builder()
         .host("localhost")
         .port(23100)
-        .auth("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI1NyIsImlhdCI6MTY2MTkyMDk4MCwiZXhwIjoyMjY4MjkxNDUzLCJkaSI6MSwiYWkiOjU3LCJhdCI6MSwidHlwZSI6ImFjY2VzcyJ9.gTzincpiq85yZdTta1vQtVRA4pDheXDsXZuO5_iQEL0")
+        .auth("YOUR_AUTH_TOKEN")
         .build()
 
     val diaryRemoteDataSource = QuestionnaireRemoteDataSourceGrpcImpl(config)
